599b4c03514be5e557c20072f3644512d099fbc0
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2024-04-19 17:11:25 -0600

    connect the API

--- src/app/components/Question.jsx
+++ src/app/components/Question.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
 const Question = () => {
+  fetch('https://restcountries.com/v3.1/all')
+  .then(response => response.json())
+  .then(data => {
+    const randomCountries = getRandomElements(data, 10);
+    createQuiz(randomCountries)
+  })
+  .catch(error => console.error('Error fetching countries:', error));
+
+  function getRandomElements(array, numberOfElements) {
+    const shuffled = array.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, numberOfElements)
+  }
+
+  function createQuiz(countries) {
+    if (!countries || countries.length === 0) {
+      console.error('No countries data available.');
+      return;
+    }
+  
+    countries.forEach(country => {
+      console.log(country.name.common);
+    });
+  }
+
   return (
-    <div className="mt-8 pr-60 pl-60 sm:pl-28 sm:pr-28">
+    <div className="mt-5 md:mr-56 md:ml-56 sm:ml-[100px] sm:mr-[100px]">
         <p className="text-xl">Which country is Kuala Lumpur the capital?</p>
     </div>
   )
